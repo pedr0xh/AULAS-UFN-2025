415782bca4dddb051eec49afb15d061bba70e018
Pedro Henrique Daroda <pedro.daroda@ufn.edu.br>
2025-11-18 15:51:45 -0300

    Update tb_distribuidor.vhd

--- SistemasDigitais/tb_distribuidor.vhd
+++ SistemasDigitais/tb_distribuidor.vhd
@@ -1,69 +1,50 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-
 
 entity tb_distribuidor is
-end entity;
-
+end tb_distribuidor;
 
 architecture sim of tb_distribuidor is
--- sinais de teste
-signal clk : std_logic := '0';
-signal rst_n : std_logic := '0';
-signal btn_manual : std_logic := '0';
-signal interval_set : std_logic_vector(7 downto 0) := (others => '0');
-signal led_wait, led_dispense, led_motor : std_logic;
-signal motor_out : std_logic;
 
+    signal clk : std_logic := '0';
+    signal reset : std_logic := '1';
 
-constant CLK_PERIOD : time := 20 ns; -- 50 MHz
+    signal motor_out : std_logic;
+    signal led_wait : std_logic;
+    signal led_dispense : std_logic;
 
+    component distribuidor
+        Port(
+            clk        : in  std_logic;
+            reset      : in  std_logic;
+            motor_out  : out std_logic;
+            led_wait   : out std_logic;
+            led_dispense : out std_logic
+        );
+    end component;
 
 begin
--- instância DUT
-dut: entity work.distribuidor_top
-generic map (
-CLK_FREQ_HZ => 50000000,
-DEFAULT_INTERVAL_SEC => 5
-)
-port map (
-clk => clk,
-rst_n => rst_n,
-btn_manual => btn_manual,
-interval_set => interval_set,
-led_wait => led_wait,
-led_dispense => led_dispense,
-led_motor => led_motor,
-motor_out => motor_out
-);
 
+    UUT: distribuidor
+        port map(
+            clk => clk,
+            reset => reset,
+            motor_out => motor_out,
+            led_wait => led_wait,
+            led_dispense => led_dispense
+        );
 
--- clock
-clk_proc : process
-begin
-while now < 500 ms loop
-clk <= '0';
-wait for CLK_PERIOD/2;
-clk <= '1';
-wait for CLK_PERIOD/2;
-end loop;
-wait;
-end process;
+    -- Clock 10ns período
+    clk <= not clk after 5 ns;
 
+    process
+    begin
+        reset <= '1';
+        wait for 20 ns;
+        reset <= '0';
 
-stim_proc: process
-begin
--- reset
-rst_n <= '0';
-wait for 100 ns;
-rst_n <= '1';
+        wait for 5 sec;  -- simula vários ciclos
+        wait;
+    end process;
 
-
--- config: intervalo curto pra simulação: 1 segundo
-interval_set <= std_logic_vector(to_unsigned(1, 8));
-
-
--- espera: timer deve expirar uma vez e ativar motor
-wait for 1200 ms; -- aguarda mais que 1s para ver o ciclo
-end architecture;
+end sim;
